d4cc86e52172566caff7d7fab15817b3a67197aa
Gowthami PV <100338896+gowthamipv28@users.noreply.github.com>
2022-03-09 20:19:53 +0530

    Update Hospitaltest.java

--- src/test/java/hospitalassignment/Hospitaltest.java
+++ src/test/java/hospitalassignment/Hospitaltest.java
@@ -1,1 +1,36 @@
-package hospitalassignment;import java.util.Calendar;import java.util.Date;import java.util.GregorianCalendar;import java.util.List;import org.testng.annotations.Test;public class Hospitaltest {    @Test    public void getData() {        Date fromDate = new GregorianCalendar(2019, Calendar.OCTOBER, 01).getTime();        Date toDate = new GregorianCalendar(2019, Calendar.OCTOBER, 30).getTime();        Hospital hospital = new HospitalService().withName("CMH").withLocation("Bangalore").build();        List<Patient> patients = new Hospital().patientsWithinTheDateRange(fromDate, toDate);        long inPatientCount = patients.stream().filter(p -> p.getLocation().equalsIgnoreCase(hospital.getLocation())).count();        long outPatientCount = patients.size() - inPatientCount;        System.out.println("Total in " + hospital.getLocation() + " -- " + inPatientCount);        System.out.println("Total Outstation -- " + outPatientCount);        Double inPatientPercentage = Double.valueOf(inPatientCount * 100 / patients.size());        Double outPatientPercentage = Double.valueOf(outPatientCount * 100 / patients.size());        System.out.println("Patients in " + hospital.getLocation() + " -- " + inPatientPercentage);        System.out.println("Patients from outside Bangalore -- " + outPatientPercentage);    }    }
+package hospitalassignment;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import org.testng.annotations.Test;
+
+public class Hospitaltest {
+    @Test
+    public void getData() {
+        Date fromDate = new GregorianCalendar(2019, Calendar.OCTOBER, 01).getTime();
+        Date toDate = new GregorianCalendar(2019, Calendar.OCTOBER, 30).getTime();
+
+
+        Hospital hospital = new HospitalService().withName("CMH").withLocation("Bangalore").build();
+
+        List<Patient> patients = new Hospital().patientsWithinTheDateRange(fromDate, toDate);
+
+
+        long inPatientCount = patients.stream().filter(p -> p.getLocation().equalsIgnoreCase(hospital.getLocation())).count();
+        long outPatientCount = patients.size() - inPatientCount;
+
+        System.out.println("Total in " + hospital.getLocation() + " -- " + inPatientCount);
+        System.out.println("Total Outstation -- " + outPatientCount);
+
+        Double inPatientPercentage = Double.valueOf(inPatientCount * 100 / patients.size());
+        Double outPatientPercentage = Double.valueOf(outPatientCount * 100 / patients.size());
+
+        System.out.println("Patients in " + hospital.getLocation() + " -- " + inPatientPercentage);
+        System.out.println("Patients from outside Bangalore -- " + outPatientPercentage);
+    }
+
+    }
+
